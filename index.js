59ed4bf8b5918ff3e75ccf0b6969be77b6a283b8
Dominic <camilldominic@gmail.com>
2024-03-25 20:50:40 -0700

    need to build server json to html js functions

---
+++ index.js
@@ -0,0 +1,168 @@
+/*
+		var user;
+		if(user == null){
+			openLogin();
+		}
+
+		function openLogin(){
+			var main = document.getElementsByClassName("main")[0];
+			var login = document.getElementsByClassName("login")[0];
+			main.style.display = "none";
+			login.style.display = "block";
+		}
+		*/
+
+		function hoverServerButton(id){
+			var element = document.getElementById(id + "Notification");
+			if(element.className === "unSeenMessage"){
+				element.className = "unSeenMessageHovered";
+			} else {
+				element.className = "SeenMessageHovered";
+			}
+			
+			//element.style.setProperty("content", "url(images/unseenMessageHovered.png)");
+			//element.style.marginTop = "20px";
+			//element.style.height = "24px";
+		}
+	
+		// Function to download data to a file
+		function download(data, filename, type) {
+			var file = new Blob([data], {type: type});
+			if (window.navigator.msSaveOrOpenBlob) // IE10+
+				window.navigator.msSaveOrOpenBlob(file, filename);
+			else { // Others
+				var a = document.createElement("a"),
+						url = URL.createObjectURL(file);
+				a.href = url;
+				a.download = filename;
+				document.body.appendChild(a);
+				a.click();
+				setTimeout(function() {
+					document.body.removeChild(a);
+					window.URL.revokeObjectURL(url);  
+				}, 0); 
+			}
+			alert(file);
+		}
+	
+		function unHoverServerButton(id){
+			var element = document.getElementById(id + "Notification");
+			if(element.className === "unSeenMessageHovered"){
+				element.className = "unSeenMessage";
+			} else {
+				element.className = "SeenMessage";
+			}
+			//element.style.setProperty("content", "url(images/unseenMessage.png)");
+			//element.style.marginTop = "27.5px";
+			//element.style.height = "8px";
+		}
+
+        async function loadDmThread(data, index){
+            let messageThread = document.querySelector(".MessageThread");
+            messageThread.innerHTML = '';
+            console.log(data.DMs[index]);
+            for(let i = 0; i<data.DMs[index].messages.length; i++){
+                let message = data.DMs[index].messages[i];
+                messageThread.appendChild(createMessage(message.userName, message.date, message.time, message.body));
+            }
+        }
+
+        async function openDmThread(id){
+            let index = id.substring(2);
+            fetch("./users/user1.json")
+				.then(res => {
+					if(!res.ok){
+						throw new Error("user file does not exist");
+					}
+					return res.json();
+				})
+				.then(data => {
+                    console.log(index);
+                    loadDmThread(data, index);
+                })
+				.catch(error => console.error(error));
+        }
+
+        async function loadDMs(data){
+            let messageList = document.querySelector(".MessageList");
+            //console.log(data.DMs.length);
+            for(let i = 0; i<data.DMs.length; i++){
+                let dm = data.DMs[i];
+                messageList.appendChild(elementFromHTML(`
+					<button class="DmThread" id="DM${i}" onclick="openDmThread(this.id)">
+                        <img src="${dm.pfp}" class="DmPFP">
+						<h2 class="DmUserName">${dm.userName}</h2>
+					</button>
+				`));
+            }
+		};
+
+		async function openDMs(){
+            let messageList = document.querySelector(".MessageList");
+            messageList.innerHTML = '';
+			fetch("./users/user1.json")
+				.then(res => {
+					if(!res.ok){
+						throw new Error("user file does not exist");
+					}
+					return res.json();
+				})
+				.then(data => loadDMs(data))
+				.catch(error => console.error(error));
+		}
+
+		function createMessage(user, date, time, body){
+			return elementFromHTML(`
+				<div class="message" style="display: inline-block;">
+					<table>
+						<tbody>
+							<tr>
+								<td style="vertical-align: top;">
+									<img class="messagePFP" id="message1PFP" style="content: url('images/dmIcon.png');">
+								</td>
+								<td>
+									<div style="vertical-align: top; overflow: auto; padding-left: 10px; ">
+										<div style="display: inline-block; overflow: hidden;">
+											<h2 class="messageUsername" id="message1Username">${user}</h2>
+											<h3 class="messageDate" id="message1Date">${date}</h3>
+											<h3 class="messageTime" id="message1Time">${time}</h3>
+										</div>
+										<p>${body}</p>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			`);
+
+		}
+
+		function elementFromHTML(html){
+            const template = document.createElement("template");
+            template.innerHTML = html.trim();
+            return template.content.firstElementChild;
+        }
+
+		class User{
+			constructor(user, pass, peerId){
+				this.user = user;
+				this.pass = pass;
+		        this.peerId = peerId;
+			}
+
+			checkPass(pass){
+				return pass === this.pass;
+			}
+
+			getUser(){
+				return this.user;
+			}
+
+			setNewPass(oldPass, newPass){
+				if(oldpass === this.pass){
+					this.pass = newPass;
+				}
+				return;
+			}
+		}
